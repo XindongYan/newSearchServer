6f40823cfbfd5aec3db7d8ef254be60ef4683f30
Addison <addison@yunio.com>
2019-05-19 19:09:29 +0800

    更改数据库名称

--- connection/index.js
+++ connection/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose.connect('mongodb://127.0.0.1:27017/news', { useNewUrlParser: true, useCreateIndex: true })
+  mongoose.connect('mongodb://127.0.0.1:27017/index', { useNewUrlParser: true, useCreateIndex: true })
 }
