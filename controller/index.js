cfc2e589991dce642315845b8570c6d0837b8568
Addison <addison@yunio.com>
2019-05-21 00:05:23 +0800

    分词

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,6 @@
 const { newModel } = require('../models/newModel');
+// 分词模块
+const Segment = require('segment');
 
 module.exports = {
   search: async (ctx, next) => {
@@ -10,6 +12,19 @@ module.exports = {
     for (const p_s of params.split(/\s+/)) {
       paramsArray.push(p_s);
     };
+
+    if (paramsArray.length === 1) {
+      const segment = new Segment();
+      segment.useDefault();
+
+      const wordArray = segment.doSegment(paramsArray[0]);
+
+      paramsArray.length = 0;
+
+      for (const w of wordArray) {
+        paramsArray.push(w.w)
+      }
+    }
 
     result = await newModel.find({ 'index.keyword': { $all: paramsArray } }, 'url title time text').exec();
     if (result.length === 0) {
@@ -23,8 +38,8 @@ module.exports = {
         if (index !== -1) {
           let text = r.text.replace(paramsArray[0], `<a style="color: red">${paramsArray[0]}</a>`);
           r.text = text;
-        }
-      }
+        };
+      };
     };
 
     ctx.body = {
